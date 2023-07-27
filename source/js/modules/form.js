4d763c95227a12fc0f435181d0edd9900320c85a
Nail Markov <markov_nail@mail.ru>
2023-07-27 10:09:59 +0300

    Проверяет на ошибки в проекте

--- source/js/modules/form.js
+++
@@ -1,8 +0,0 @@
-const inputElement = document.querySelector('[data-on-input-validate]');
-const messageElement = document.querySelector('[data-message-base="Сообщение"]');
-
-export const initForm = (element) => {
-  const validateInput = window.form.validateFormElement(inputElement, messageElement);
-
-  return validateInput;
-};
